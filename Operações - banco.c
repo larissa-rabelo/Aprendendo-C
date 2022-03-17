df038e91c11fc76de75e7cc769003927a05ea270
Larissa Rabelo <91967060+larissa-rabelo@users.noreply.github.com>
2022-03-17 19:37:58 -0300

    Add files via upload

---
+++ Operações - banco.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(void){
+   int n;
+   printf("\n(1) para Saldo\n(2) para Extrato\n(3) para Saque\n(4) para Sair");
+   printf ("\n Informe a operação desejada: ");
+   
+   scanf ("%d", &n);
+   
+   switch(n) {
+       case 1:
+       printf("\nSaldo");
+       break;
+       
+       case 2:
+       printf("\nExtrato");
+       break;
+       
+       case 3:
+       printf("\nSaque");
+       break;
+       
+       case 4:
+       printf("\nSair");
+       break;
+       
+default:
+    printf("\nInválido");
+   }
+   
+   return 0;
+}
