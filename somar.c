df6f25c890a115e2a179576783223486580d88e0
Larissa Rabelo <91967060+larissa-rabelo@users.noreply.github.com>
2022-03-22 20:16:34 -0300

    Add files via upload

---
+++ somar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int somar(){
+    return 2 +3; 
+}
+int main(){
+    int resultado=0;
+    resultado = somar();
+    printf("O resultadoda funcao é =%d", resultado);
+    return 0;
+}
